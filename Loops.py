c6041c1a9516a6d4cc19de729ddd7e2dfb00d80a
alisoncohen <alison@planetcohen.com>
2016-10-05 09:15:48 -0700

    Loops and Math Games

--- Loops.py
+++ Loops.py
@@ -60,14 +60,20 @@ print "mult(0,0)     0 ==", mult(0,0)
 
 #problem four
 def dot( L, K ):
+
     """
     input: two lists
     output: the dot product of the lists
     """
 
-    result = 1
-    for i in range(0 ,L[:]):
-        
+
+    result = 0
+    for i in range(len(L)):
+        if len(L) == len(K):
+            result = result + L[i]*K[i]
+    return result
+
+
 
 
 
@@ -76,3 +82,60 @@ print "dot( [1,2,3,4], [10,100,1000,10000] )  43210.0 ==", dot( [1,2,3,4], [10,1
 print "dot( [5,3], [6] )        0.0 ==", dot( [5,3], [6] )
 print "dot( [], [6] )           0.0 ==", dot( [], [6] )
 print "dot( [], [] )            0.0 ==", dot( [], [] )
+
+
+
+#problem five
+def count_evens(L):
+    """
+    input: a list
+    output: the number of evens
+    """
+
+    result = 0
+
+    for i in L:
+        if i%2 == 0:
+            result = result + 1
+    return result
+
+
+print "count_evens([2, 1, 2, 3, 4], 3 == ", count_evens([2, 1, 2, 3, 4])
+print "count_evens([2, 2, 0]), 3 == ", count_evens([2, 2, 0])
+print "count_evens([1, 3, 5]), 0 == ", count_evens([1, 3, 5])
+
+#problem six
+def count9(L):
+    """
+    input: a list
+    output: the number of 9s
+    """
+
+    result = 0
+
+    for i in L:
+        if i == 9:
+            result = result + 1
+    return result
+
+print "count9([1, 2, 9]), 1 == ",count9([1, 2, 9])
+print "count9([1, 9, 9]), 2 == ",count9([1, 9, 9])
+print "count9([1, 9, 9, 3, 9]), 3 == ",count9([1, 9, 9, 3, 9])
+
+
+#challenge problems
+
+#problem one
+def printRect(W, H, S):
+    """
+    input: a width, a height and a symbol
+    output: a square of the symbols with the dimensions given
+    """
+
+    result = 0
+
+    for i in H:
+
+        W = 
+
+printRect( 4, 6, '%' )
---
+++ MultiplicationPracticeGame.py
@@ -0,0 +1,41 @@
+def play():
+    import random
+
+    high = 2
+
+    for j in range(2):
+        for i in range(5):
+
+            print "high: ", high
+
+            factor1 = random.r  andint(0,10)
+            factor2 = random.randint(0,high)
+            userAnswer = -1
+            correctAnswer = factor1*factor2
+
+            while userAnswer != correctAnswer:
+                userAnswer = raw_input("Please enter the product of " +str(factor1) + " and " +str(factor2) + ":")
+                try:
+                     userAnswer = int(userAnswer)
+                    if userAnswer == correctAnswer:
+                        print "Correct"
+
+                        if j == 1:
+                            high += 5
+
+                    else:
+                        print "Incorrect"
+                except:
+                    print "Enter an integer"
+
+
+def instructions():
+    print "Hello!"
+    name = raw_input("what is your name?")
+    print "Nice to meet you" ,name,". Let's practice multiplication! I will give you two integers and you give me the product"
+
+def main():
+    instructions()
+    play()
+
+main()
