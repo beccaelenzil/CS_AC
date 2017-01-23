51e7507ebd1d647f079cea6a2f201762f32428cb
alisoncohen <alison@planetcohen.com>
2017-01-22 16:06:49 -0800

    add BLUE

--- ScoringTheGame/city_game/CityGames.py
+++ ScoringTheGame/city_game/CityGames.py
@@ -4,8 +4,9 @@ import random
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 
 class Building(pygame.sprite.Sprite):
