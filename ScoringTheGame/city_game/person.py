18e86e2d2d7b63a27d0988e0b48084c1213d0d11
alisoncohen <alison@planetcohen.com>
2017-02-13 14:57:27 -0800

    fix position function

--- ScoringTheGame/city_game/person.py
+++ ScoringTheGame/city_game/person.py
@@ -13,18 +13,16 @@ class Person():
         self.image = pygame.image.load("images/dude.png").convert()
         self.image.set_colorkey(WHITE)
         self.velocity = 0
-        self.acceleration = -9.8
+        self.acceleration = 2
 
     def move(self, delta = 0.5):
         self.x += delta
 
     def jump(self):
-        self.acceleration = 5
+        self.acceleration = -5
 
     def update_position(self, dt = 1):
-        self.y = 0.5 * self.acceleration * (dt)(dt) + self.velocity * dt + self.y
-
-
+        self.y = 0.5 * self.acceleration * (dt) + self.velocity * dt + self.y
 
     def draw(self, screen):
         screen.blit(self.image, [self.x, self.y])
